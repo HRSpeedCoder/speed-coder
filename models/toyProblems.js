5db9933a68e08fc6357870b0fad54abeb7bf01f8
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-15 01:08:25 +0200

    added a query

--- models/toyProblems.js
+++ models/toyProblems.js
@@ -1,1 +1,16 @@
-const db = require('../database')
+const db = require('../database');
+
+module.exports = {
+
+  getProblem: (id, callback) => {
+    const query = `SELECT * FROM problem WHERE id = ${id}`
+    db.query(query, (err, data) => {
+      if (err) {
+        console.log('error in models/toyProblems.getProblem')
+        throw err;
+      } else {
+        callback(err, data);
+      }
+    });
+  }
+}
