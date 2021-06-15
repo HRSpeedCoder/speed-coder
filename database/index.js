5a60c95775bb9ece8a97d907a7e67c3b7281cd48
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-15 23:52:50 +0200

    added an index.js file to database.

---
+++ database/index.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+  user: 'speedcoder',
+  password: 'speedcoder',
+  database: 'toy-problems'
+});
+
+db.connect();
+
+module.exports = db;
